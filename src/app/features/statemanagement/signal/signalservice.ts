942037bc5406050669e899f54de53f353194a0b0
Amirsuhail Salim Hundekari <suhail1680@gmail.com>
2025-12-21 11:18:19 +0530

    #15 commit 1.signal and computed signal.

--- src/app/features/statemanagement/signal/signalservice.ts
+++ src/app/features/statemanagement/signal/signalservice.ts
@@ -1,19 +1,64 @@
+import { HttpClient } from '@angular/common/http';
 import { computed, Injectable, signal } from '@angular/core';
-
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class Signalservice {
-  private count = signal(0)
+  private count = signal(0);
+   todo = signal<Todo[]>([]);
+   loading = signal(false);
 
-  // double the count
-  doublecount = computed(()=>this.count()*2)
+  // double the count computer signals
+  doublecount = computed(() => this.count() * 2);
+  completedTodos:any = computed(() => {
+    this.todo().filter((todos) => todos.completed);
+  });
+  pendingTodos:any = computed(() => {
+    this.todo().filter((todos) => !todos.completed);
+  });
 
-  increment(){
-    this.count.update(count=>count++)
+  constructor(private http: HttpClient) {}
+
+  increment() {
+    this.count.update((count) => count + 1);
   }
 
-  decrement(){
-    this.count.update(count=>count--)
+  decrement() {
+    this.count.update((count) => count - 1);
+  }
+  reset() {
+    this.count.update((count) => (count = 0));
+  }
+  getCount() {
+    return this.count;
+  }
+
+  async fetchTodos(){
+    this.loading.set(true)
+    try{
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json()
+      this.todo.set(data)
+    }finally{
+     this.loading.set(false)
+    }
+  }
+
+  addtodo(title:string){
+     const newTodo:Todo ={
+      id:Date.now(),
+      title,
+      completed:false
+     }
+     this.todo.update(current=>[...current,newTodo])
+  }
+
+  toggleTodo(id:number){
+    this.todo.update(todos=>todos.map(todo=>todo.id === id ? {...todo,completed:!todo.completed }:todo))
   }
 }
--- src/app/features/statemanagement/statemanagement.ts
+++ src/app/features/statemanagement/statemanagement.ts
@@ -1,45 +1,58 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { RxjsandobservablesService } from '../rxjsandobservables/service/rxjsandobservables';
-import { MatCard, MatCardTitle, MatCardFooter } from "@angular/material/card";
-import { MatFormField } from "@angular/material/input";
+import { MatCard, MatCardTitle, MatCardFooter } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { StateService } from './state-service';
+import { Signalservice } from './signal/signalservice';
 
 @Component({
   selector: 'app-statemanagement',
-  imports: [MatCard, MatFormField, MatCardTitle, MatCardFooter,MatTableModule,CommonModule],
+  imports: [MatCard, MatCardTitle, MatCardFooter, MatTableModule, CommonModule],
   templateUrl: './statemanagement.html',
   styleUrl: './statemanagement.scss',
 })
-export class Statemanagement implements OnInit{
+export class Statemanagement implements OnInit {
   // using beh subject
-  userService = inject(RxjsandobservablesService)
-  stateService = inject(StateService)
+  userService = inject(RxjsandobservablesService);
+  stateService = inject(StateService);
+  signalService = inject(Signalservice);
   user$ = this.stateService.user$;
 
-// using signals
+  // using signals
 
-
-
-  ngOnInit(): void {
-     this.userService.getUserData().subscribe(users => {
-      console.log('User Data from Component 1:', users);
-    });
-
-     this.stateService.setUser({
-      id: 1,
-      name: 'John Doe',
-      email: 'john@example.com'
+  constructor() {
+    effect(() => {
+      // console.log('Todos changed:', this.signalService.fetchTodos());
     });
   }
 
+  ngOnInit(): void {
+    this.userService.getUserData().subscribe((users) => {
+      console.log('User Data from Component 1:', users);
+    });
 
-  setNewUser(){
-     this.stateService.setUser({
+    this.stateService.setUser({
+      id: 1,
+      name: 'John Doe',
+      email: 'john@example.com',
+    });
+    this.signalService.fetchTodos();
+  }
+  addTodo(title: string) {
+    this.signalService.addtodo(title);
+  }
+
+  toggleTodo(id: number) {
+    this.signalService.toggleTodo(id);
+  }
+
+  setNewUser() {
+    this.stateService.setUser({
       id: 2,
       name: 'Amirsuhail',
-      email: 'amir@example.com'
+      email: 'amir@example.com',
     });
   }
 }
