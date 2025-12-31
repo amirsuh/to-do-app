c8f0735015f20a3efd8797c4b56bfd9cc68498ab
Amirsuhail Salim Hundekari <suhail1680@gmail.com>
2025-12-31 13:24:07 +0530

    # commit for Api integration for login

--- src/app/core/services/singleton/singleton.ts
+++ src/app/core/services/singleton/singleton.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'platform',
+  providedIn: 'root',
 })
 export class Singleton {
-  varTocheckSingleton:number = 0
+  varTocheckSingleton: number = 0;
+  user: any;
   private counter: number = 0;
-
+  private http = inject(HttpClient);
 
   log(message: string): void {
     console.log(`[LOG]: ${message}`);
@@ -18,5 +21,38 @@ export class Singleton {
   getCounter(): number {
     return this.counter;
   }
-
+  login(username: string, password: string): Observable<boolean> {
+    if (username === 'admin' && password === 'admin') {
+      return this.fetchUserData(username).pipe(
+        map((userData) => this.user = userData
+          //return of({userData:this.user,status:'true'});
+        )
+      );
+    }
+    return of(false);
+  }
+  fetchUserData(username: string): Observable<any> {
+    const body = {
+    username: 'emilys',       // or use the username parameter
+    password: 'emilyspass',
+    expiresInMins: 30
+  };
+      return this.http.post('https://dummyjson.com/auth/login', body, {
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: false // ✅ equivalent to credentials: 'include'
+  });
+    // return this.http.post('https://dummyjson.com/auth/login', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({
+    //     username: 'emilys',
+    //     password: 'emilyspass',
+    //     expiresInMins: 30, // optional, defaults to 60
+    //   }),
+    //   credentials: 'include', // Include cookies (e.g., accessToken) in the request
+    // });
+  }
+  isAuthenticated(): boolean {
+    return this.user !== null;
+  }
 }
