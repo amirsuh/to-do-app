b6b95ada0856fc25dd2b2d1d3f69b4f3498eefdd
Amirsuhail Salim Hundekari <suhail1680@gmail.com>
2026-01-01 10:54:00 +0530

    #35 commit for best practise with ngondestroy using subject

--- src/app/features/routing/component/admin/admin.ts
+++ src/app/features/routing/component/admin/admin.ts
@@ -1,24 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy } from '@angular/core';
 import { Singleton } from '../../../../core/services/singleton/singleton';
 import { CommonModule } from '@angular/common';
 import { Main } from "../component/main/main";
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
   imports: [CommonModule, Main],
   templateUrl: './admin.html',
   styleUrl: './admin.scss',
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class Admin {
+export class Admin implements OnDestroy{
   @Input() title: string = '';  // Title for the card
   @Input() description: string = '';  // Description for the card
   @Input() buttons: any[] = [];  // Array of buttons (can be of type {label: string, action: Function})
-
+ private destroy$ = new Subject<void>();
   constructor(private single:Singleton){
+    //Freeze Your Data (Yes, Really)
+    const frozenData = Object.freeze({ name: 'Angular', version: 17 });
    single.increment()
    this.single.log('admin')
    console.log(this.single.varTocheckSingleton,single.getCounter())
-   this.single.login('admin','admin').subscribe(res=>{
+   this.single.login('admin','admin').pipe(takeUntil(this.destroy$)).subscribe(res=>{
     console.log(res)
    },err => {
     // This runs on error
@@ -29,4 +33,9 @@ export class Admin {
     console.log('Completed');
   })
   }
+
+  ngOnDestroy() {
+  this.destroy$.next();
+  this.destroy$.complete();
+}
 }
